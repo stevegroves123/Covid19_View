206328ce4052ef867627f1af28c0ae02757ea472
Steven Groves <stevegroves123@gmail.com>
2020-06-13 17:49:29 +0100

    Updated the icon for the iPhone and the iPad

--- Covid19_View/ContentView.swift
+++ Covid19_View/ContentView.swift
@@ -18,23 +18,26 @@ struct ContentView: View {
             Form {
                 Picker(selection: $selectedCountry, label: Text("Country:").font(.headline)) {
                     ForEach(0..<countries.count) {
-                        Text(self.countries[$0])
+                        Text(self.countries[$0].replacingOccurrences(of: "_", with: " "))
+                            .font(.headline)
                     }
                 }
             List (records.filter({covid in covid.countriesAndTerritories == self.countries[self.selectedCountry]}), id: \.id) {covid in
-                Text("\(covid.dateRep)")
-                Text(" Cases:\(covid.cases ?? "0")")
+                Text("\(covid.day)/\(covid.month)")
+                Text("Cases:\(covid.cases ?? "0")")
                 if Int(covid.deaths)! > 1000 {
-                    Text(" Deaths:\(covid.deaths )").foregroundColor(.red)
+                    Text("Deaths:\(covid.deaths )").foregroundColor(.red)
                 } else if Int(covid.deaths)! >= 500 && Int(covid.deaths)! < 1000 {
-                    Text(" Deaths:\(covid.deaths )").foregroundColor(.orange)
+                    Text("Deaths:\(covid.deaths )").foregroundColor(.orange)
                 } else if Int(covid.deaths)! >= 0 && Int(covid.deaths)! < 201 {
-                    Text(" Deaths:\(covid.deaths )").foregroundColor(.green)
+                    Text("Deaths:\(covid.deaths )").foregroundColor(.green)
                 } else {
-                    Text(" Deaths:\(covid.deaths)")
+                    Text("Deaths:\(covid.deaths)")
                     }
+                Text("Sum:\(Int(covid.cases ?? "0")! + Int(covid.deaths)!)")
                 }
             }.navigationBarTitle("Covid19: \(self.countries[self.selectedCountry].replacingOccurrences(of: "_", with: " "))")
+                .font(.system(size: 16))
         }
         .onAppear(perform: loadData)
     }
